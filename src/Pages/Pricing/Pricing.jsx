4f65775bbce88a2c62ba9d470ae2b742f3af838d
Md Fuad Amir <mdfuadamir@gmail.com>
2024-07-03 20:44:22 +0600

    added pricing section

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,8 +1,16 @@
-
+import Packege from "../../Components/Packege/Packege";
+import background from"../../assets/pricing.jpg"
 
 const Pricing = () => {
     return (
         <div>
+            <div style={{backgroundImage:`url(${background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}} className="bg-center">
+                <div className="bg-black bg-opacity-50 px-4 md:px-36 py-36">
+                <p className="text-xl text-white font-sans">30 days money back guarantee</p>
+                <h1 className="mt-2 font-bold font-sans text-2xl md:text-5xl text-white">No Extra Fees. Friendly Support</h1>
+                </div>
+            </div>
+            <Packege/>
             
         </div>
     );
